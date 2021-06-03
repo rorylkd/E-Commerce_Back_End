02016d5ec48af678025a93a125207fe0cd2a0200
rorylkd <rory.donaldson14@gmail.com>
2021-06-03 15:17:28 +0800

    Started adding associations on the model index.js file

--- models/index.js
+++ models/index.js
@@ -3,10 +3,18 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
+const { hasMany } = require('./Product');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+})
 
 // Products belongToMany Tags (through ProductTag)
 
