0f5e4f5ee390ac9dca4fda7ccb3bf16c29f00428
rorylkd <rory.donaldson14@gmail.com>
2021-06-03 16:23:56 +0800

    Added route to find one category by its id value

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -17,7 +17,25 @@ router.get("/", async (req, res) => {
   // be sure to include its associated Products
 });
 
-router.get("/:id", (req, res) => {
+
+router.get("/:id", async (req, res) => {
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{
+        model: Product
+      }],
+    });
+
+    if (!categoryData) {
+      res.status(404).json({message: 'No category found with that id!'});
+      return
+    }
+
+    res.status(200).json(categoryData);
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // find one category by its `id` value
   // be sure to include its associated Products
 });
