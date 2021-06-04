de555e40252c35fe9ec91d29eafa5252b0920cbd
rorylkd <rory.donaldson14@gmail.com>
2021-06-04 10:24:20 +0800

    Added update method for tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -57,7 +57,20 @@ router.post("/", (req, res) => {
   // create a new tag
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id", async (req, res) => {
+  try {
+    const tagData = await Tag.update(
+      { tag_name: req.body.tag_name },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+    return res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // update a tag's name by its `id` value
 });
 
